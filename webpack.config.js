7f64112dd42fb519e3aa6d65515460e5f73f0fbb
“Irina <irina.osipova2012@gmail.com>
2023-06-09 18:41:58 +0200

    fix: fix path, add additional settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
-            { test: /\.ts$/, use: 'ts-loader' },
+            { test: /\.ts$/i, use: 'ts-loader' },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
@@ -28,6 +29,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
     ],
 };
 
