38d3756ce83f6040b0c551dae3f5bd3152913ad9
“Irina <irina.osipova2012@gmail.com>
2023-06-10 18:30:54 +0200

    fix: fix linter errors

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,15 +1,15 @@
-export interface NewsData {
-  author: string;
-  title: string;
-  description: string;
-  url: string;
-  urlToImage: string;
-  publishedAt: string;
-  content: string;
-  source: SourceName[];
+export default interface NewsData {
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+    source: SourceName;
 }
 
 interface SourceName {
-  id: string;
-  name: string;
+    id: string;
+    name: string;
 }
