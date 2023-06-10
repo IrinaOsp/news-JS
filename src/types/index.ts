c74293ae7bf5accabc0caab7ff60b016a08f1b5c
“Irina <irina.osipova2012@gmail.com>
2023-06-10 21:01:46 +0200

    feat: add Sources interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,3 +13,20 @@ export interface SourceName {
     id: string;
     name: string;
 }
+
+export interface TopNewsData {
+    articles: NewsData[] | never[];
+}
+
+export interface NewsSources {
+    sources: SourcesData[] | never[];
+}
+
+export interface SourcesData extends SourceName {
+    category: string;
+    country: string;
+    description: string;
+    language: string;
+    name: string;
+    url: string;
+}
