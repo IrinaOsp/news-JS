70b8ae089ee33452869eae0eb41bbd8f1fd87664
“Irina <irina.osipova2012@gmail.com>
2023-06-11 15:40:17 +0200

    feat: migrate appLoader to TS

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'ee3d4d2936204743a0e50e04ccb8f2a1', // получите свой ключ https://newsapi.org/    ee3d4d2936204743a0e50e04ccb8f2a1
+            apiKey: 'ee3d4d2936204743a0e50e04ccb8f2a1', // получите свой ключ https://newsapi.org/
         });
     }
 }
