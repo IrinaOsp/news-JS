cb5a748daa048f6da90f97311802ec5d37901685
“Irina <irina.osipova2012@gmail.com>
2023-06-11 20:55:03 +0200

    feat: migrate controller to TS

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,7 +1,7 @@
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: () => void) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,14 +10,14 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
+    getNews(e: Event, callback: () => void) {
         let target = e.target;
         const newsContainer = e.currentTarget;
 
         while (target !== newsContainer) {
-            if (target.classList.contains('source__item')) {
+            if (target instanceof HTMLElement && target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                if (newsContainer instanceof HTMLElement && newsContainer.getAttribute('data-source') !== sourceId && sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
                         {
@@ -31,7 +31,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            if (target instanceof HTMLElement) target = target.parentNode;
         }
     }
 }
